982db5a238fc03123e6ae9b6faf0b773bb042def
kato atsushi <ka.baseball1997@gmail.com>
2021-04-18 19:11:08 +0900

    appointment new

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -34,42 +34,92 @@ class AppointmentsController < ApplicationController
     times = make_time_schedule_in_one_day(params[:year], params[:month], params[:day])
     fitness = Fitness.find(params["fitness_id"].to_i)
     store = Store.find(params["store_id"].to_i)
-    @main = []
+    response = []
     trainers = Trainer.where(company_id: fitness.company_id)
+
     times.each do |t|
-      number_of_seats = 0
-      trainers.each do |trainer|
-        ok = false
-        # 要望のセッションメニューが当トレーナのできるセッションメニューに含まれているかのok
-        trainer.fitnesses.each do |f|
-          if f.id == fitness.id
-            ok = true
+      # 整体が予約されて、存在するトレーナーが[整体、トレーニング]をできる場合、このトレーナーの枠を使ってしまっているので新規顧客は予約をできない
+      # 管理者のスケジュールを探す
+      admins_reserved_count = BlackSchedule.where(store_id: store.id)
+      .merge((BlackSchedule.where("? < not_free_time_start AND not_free_time_start < ?", t[0], t[1])
+        .or(BlackSchedule.where("? < not_free_time_finish AND not_free_time_finish < ?", t[0], t[1])))
+        .or(BlackSchedule.where("not_free_time_start <= ? AND ? <= not_free_time_finish ", t[0], t[1]))).count
+       # トレーナーで今回のメニュー兼過去の予約のメニューを持っている者を抽出
+      reserved_datas = Appointment.where(store_id: store.id).where(appointment_time: t[0])
+      trainers_fitness_menu_ids = []
+      reserved_menu_id = []
+      trainers.each do |this_trainer|
+        count = this_trainer.trainer_shifts.where("start <= ? AND ? <= finish", t[0], t[1]).where(store_id: store.id).count
+        if count >= 1
+          fitnesses_ids = []
+          this_trainer.fitnesses.each do |f|
+            fitnesses_ids << f.id
           end
-        end
-        if ok
-          # 顧客様の希望店舗=トレーナーの指定日のシフトの店舗かつ希望セッションを行うことができる場合カウント
-          number_of_seats +=  trainer.trainer_shifts.where("start <= ? AND  ? <= finish", t[0], t[1]).where(store_id:  store.id).count
+          # 本時間に勤務予定のトレーナーの扱えるメニューを配列で取り込む
+          trainers_fitness_menu_ids << fitnesses_ids
         end
       end
-      #　トレーナーの人数が部屋数を超えていた場合、部屋数を予約可能枠数とする
-      number_of_seats > store.number_of_rooms  ? number_of_seats = store.number_of_rooms : number_of_seats
-      number_of_reserved = Appointment.where(store_id: store.id, fitness_id: fitness.id)
-                                      .where(appointment_time: t[0]).count     
-      number_of_admins = BlackSchedule.where(store_id: store.id)
-        .merge((BlackSchedule.where("? < not_free_time_start AND not_free_time_start < ?", t[0], t[1])
-          .or(BlackSchedule.where("? < not_free_time_finish AND not_free_time_finish < ?", t[0], t[1])))
-          .or(BlackSchedule.where("not_free_time_start <= ? AND ? <= not_free_time_finish ", t[0], t[1]))).count
-      free = number_of_seats - number_of_reserved - number_of_admins
-      #　空き状況が0未満の際は0とする
-      free < 0  ? free = 0 : free
+      # 部屋の可能使用数 = 部屋数-管理者のスケジュール数
+      available_room_num = store.number_of_rooms - admins_reserved_count
+      # トレーナのシフトの
+      trainers_fitness_menu_len = trainers_fitness_menu_ids.length
+      trainers_fitness_menu_len > available_room_num ? trainers_fitness_menu_len = available_room_num : trainers_fitness_menu_len
+
+      if (reserved_datas.length != 0 )
+        # すでに予約がある場合
+        reserved_datas.each do |rdata|
+          counter = 0
+          trainers_fitness_menu_ids.each do |menu_ids|
+            if(fitness.id != rdata.fitness_id)
+              if (menu_ids.include?(rdata.fitness_id) && menu_ids.include?(fitness.id))
+                trainers_fitness_menu_ids.delete_at(counter)
+                trainers_fitness_menu_len = trainers_fitness_menu_len - 1
+                break 
+              end
+            else
+              trainers_fitness_menu_ids.delete_at(counter)
+              trainers_fitness_menu_len = trainers_fitness_menu_len - 1
+              break 
+            end
+            counter = counter + 1
+          end
+        end
+        
+      end
+
+      # free = trainers_fitness_menu_len
+      # number_of_seats = 0
+      # reserved = 0
+      # trainers.each do |trainer|
+      #   ok = false
+      #   # 要望のセッションメニューが当トレーナのできるセッションメニューに含まれているかのok
+      #   trainer.fitnesses.each do |f|
+      #     f.id == fitness.id ? ok = true : ok = false
+      #   end
+      #   if ok
+      #     # 顧客様の希望店舗=トレーナーの指定日のシフトの店舗かつ希望セッションを行うことができる場合カウント
+      #     number_of_seats +=  trainer.trainer_shifts.where("start <= ? AND  ? <= finish", t[0], t[1]).where(store_id:  store.id).count
+      #   end
+      # end
+      # #　トレーナーの人数が部屋数を超えていた場合、部屋数を予約可能枠数とする
+      # number_of_seats > store.number_of_rooms ? number_of_seats = store.number_of_rooms : number_of_seats
+      # number_of_reserved = Appointment.where(store_id: store.id, fitness_id: fitness.id)
+      #                                 .where(appointment_time: t[0]).count
+      # number_of_admins = BlackSchedule.where(store_id: store.id)
+      #   .merge((BlackSchedule.where("? < not_free_time_start AND not_free_time_start < ?", t[0], t[1])
+      #     .or(BlackSchedule.where("? < not_free_time_finish AND not_free_time_finish < ?", t[0], t[1])))
+      #     .or(BlackSchedule.where("not_free_time_start <= ? AND ? <= not_free_time_finish ", t[0], t[1]))).count
+      # free = number_of_seats - number_of_reserved - number_of_admins
+      # #　空き状況が0未満の際は0とする
+      # free < 0  ? free = 0 : free
       t[0].min == 0 ? start_min = "00" : start_min = t[0].min.to_s
       t[1].min == 0 ? finish_min = "00" : finish_min = t[1].min.to_s
       start_time = [t[0].hour.to_s, start_min]
       finish_time = [t[1].hour.to_s, finish_min]
-      @main << [t, free, [start_time , finish_time]]
+      response << [t, trainers_fitness_menu_len, [start_time , finish_time]]
     end
 
-    render json: @main
+    render json: response
   end
 
   def create    
