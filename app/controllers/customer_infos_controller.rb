fce897cd8c185bf5fdb5c0492478f449dc890a9c
kato atsushi <ka.baseball1997@gmail.com>
2021-05-01 11:57:18 +0900

    ss

--- app/controllers/customer_infos_controller.rb
+++ app/controllers/customer_infos_controller.rb
@@ -8,11 +8,15 @@ class CustomerInfosController < ApplicationController
     # customer = Customer.left_joins(:customer_info).select("*")
     t = Date.today
     this_month_first = DateTime.new(t.year, t.month, 1)
+    
+    # binding.pry
+    
     customer_status = customer.customer_status
     customer_record_count = customer.appointments.where(finish: true).count
     appo_count = customer.appointments.where(finish: false).count
     # 今月のすでに予約したカウント数
-    session_count = customer.appointments.where(finish: true).where("appointment_time >= ?", this_month_first).count
+    session_count = customer.appointments.where(finish: true).where("appointments.appointment_time >= ?", this_month_first).count
+    appointment_count = customer.appointments.where(finish: false).where("appointments.appointment_time >= ?", this_month_first).count
     customer_info = customer.customer_info
     render json: {
       customer: customer,
@@ -20,7 +24,8 @@ class CustomerInfosController < ApplicationController
       customer_info: customer_info,
       customer_record_len: customer_record_count,
       appo_count: appo_count,
-      session_count: session_count
+      session_count: session_count,
+      appointment_count: appointment_count
     }
   end
   # GET /customer_infos
