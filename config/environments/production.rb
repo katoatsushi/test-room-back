d518dd6f557a943e9875db2f480d16253ae04d3e
kato atsushi <ka.baseball1997@gmail.com>
2021-05-17 14:38:21 +0900

    ss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   config.action_mailer.default_options = { from: mail }
   # config.action_mailer.default_url_options = { host: 'localhost:3001' }
-  config.action_mailer.default_url_options = { host: 'https://test-room-app.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://test-room-app.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
